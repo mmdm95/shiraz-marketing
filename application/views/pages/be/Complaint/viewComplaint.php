9968d097a70bac81787895fcb479dae4a79c09c7
saeed-gerami <saeedgerami72@gmail.com>
2020-03-23 19:00:08 +0430

    4 Farvardin

---
+++ application/views/pages/be/Complaint/viewComplaint.php
@@ -0,0 +1,116 @@
+<!-- Main navbar -->
+<?php $this->view("templates/be/mainnavbar", $data); ?>
+<!-- /main navbar -->
+<!-- Page container -->
+<div class="page-container">
+    <!-- Page content -->
+    <div class="page-content">
+        <input type="hidden" id="BASE_URL" value="<?= base_url(); ?>">
+
+        <!-- Main sidebar -->
+        <?php $this->view("templates/be/mainsidebar", $data); ?>
+        <!-- /main sidebar -->
+        <!-- Main content -->
+        <div class="content-wrapper">
+            <!-- Page header -->
+            <div class="page-header page-header-default"
+                 style="border-top: 1px solid #ddd; border-left: 1px solid #ddd; border-right: 1px solid #ddd;">
+                <div class="page-header-content border-bottom border-bottom-success">
+                    <div class="page-title">
+                        <h5>
+                            <i class="icon-circle position-left"></i> <span
+                                    class="text-semibold">پیام‌های دریافتی</span>
+                        </h5>
+                    </div>
+                </div>
+                <div class="breadcrumb-line">
+                    <ul class="breadcrumb">
+                        <li>
+                            <a href="<?= base_url(); ?>admin/index">
+                                <i class="icon-home2 position-left"></i>
+                                داشبورد
+                            </a>
+                        </li>
+                        <li>
+                            <a href="<?= base_url(); ?>admin/manageContactUs">
+                                مدیریت پیام‌ها
+                            </a>
+                        </li>
+                        <li class="active">مشاهده پیام</li>
+                    </ul>
+                </div>
+            </div>
+            <!-- /page header -->
+            <!-- Content area -->
+            <div class="content">
+                <!-- Centered forms -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="panel panel-white">
+                                    <div class="panel-heading">
+                                        <h6 class="panel-title ">مشاهده پیام</h6>
+                                        <div class="heading-elements">
+                                            <a id="delContactBtn"
+                                               class="btn btn-default btn-rounded heading-btn-group border-danger-600 text-danger-600 p-10"
+                                               title="حذف" data-popup="tooltip">
+                                                <input type="hidden" value="">
+                                                <i class="icon-trash" aria-hidden="true"></i>
+                                            </a>
+                                        </div>
+
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <h5>
+                                                    <i class="icon-envelop5 position-left bg-orange btn-rounded p-10"
+                                                       style="width: 45px; height: 45px; font-size: 24px;"></i>
+                                                    <a href="" class="display-inline-block">
+                                                        <div>
+                                                        </div>
+                                                    </a>
+                                                    <span class="text-muted text-small display-inline-block">
+                                                        <i class="icon-dash" aria-hidden="true"></i>
+                                                    </span>
+                                                </h5>
+                                                <h6>
+                                                    <i class="icon-basket position-left bg-purple btn-rounded p-10"
+                                                       style="width: 45px; height: 45px; font-size: 24px;"></i>
+                                                    <small>
+                                                        شماره سفارش:
+                                                    </small>
+                                                    <span class="text-bold text-primary">
+                                                    </span>
+                                                </h6>
+                                            </div>
+                                            <div class="col-lg-12 col-md-12">
+                                                <div class="col-lg-12 alert-info p-15">
+                                                </div>
+                                            </div>
+                                            <div class="col-md-12">
+                                                <div class="col-lg-12 jumbotron pr-20 pl-20">
+                                                    <p class="text-black text-light"
+                                                       style="font-size: 15px; line-height: 26px;">
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Footer -->
+                <?php $this->view("templates/be/copyright", $data); ?>
+                <!-- /footer -->
+            </div>
+            <!-- /content area -->
+        </div>
+        <!-- /main content -->
+    </div>
+    <!-- /page content -->
+</div>
+<!-- /page container -->
---
+++ application/views/pages/be/Slider/editSlide.php
@@ -0,0 +1,190 @@
+<?php defined('BASE_PATH') OR exit('No direct script access allowed'); ?>
+
+<!-- Main navbar -->
+<?php $this->view("templates/be/mainnavbar", $data); ?>
+<!-- /main navbar -->
+<!-- Page container -->
+<div class="page-container">
+    <!-- Page content -->
+    <div class="page-content">
+        <input type="hidden" id="BASE_URL" value="<?= base_url(); ?>">
+
+        <!-- Main sidebar -->
+        <?php $this->view("templates/be/mainsidebar", $data); ?>
+        <!-- /main sidebar -->
+        <!-- Main content -->
+        <div class="content-wrapper">
+            <!-- Page header -->
+            <div class="page-header page-header-default"
+                 style="border-top: 1px solid #ddd; border-left: 1px solid #ddd; border-right: 1px solid #ddd;">
+                <div class="page-header-content border-bottom border-bottom-success">
+                    <div class="page-title">
+                        <h5>
+                            <i class="icon-circle position-left"></i>
+                            <span class="text-semibold">مشاهده محصولات</span>
+                        </h5>
+                    </div>
+                </div>
+                <div class="breadcrumb-line">
+                    <ul class="breadcrumb">
+                        <li>
+                            <a href="<?= base_url(); ?>admin/index">
+                                <i class="icon-home2 position-left"></i>
+                                داشبورد
+                            </a>
+                        </li>
+                        <li class="active">مشاهده محصولات</li>
+                    </ul>
+                </div>
+            </div>
+            <!-- /page header -->
+            <!-- Content area -->
+            <div class="content">
+                <!-- Centered forms -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <form action="" method="post">
+
+                                    <div class="panel panel-body border-top-primary">
+                                        <h6 class="no-margin text-semibold text-center">
+                                            افزودن اسلاید
+                                        </h6>
+                                        <p class="text-muted content-group-sm text-center">
+                                        </p>
+                                        <div class="row">
+                                            <div class="alert alert-danger">
+                                                <ul>
+                                                    <li>
+
+                                                    </li>
+                                                </ul>
+                                            </div>
+
+                                            <div class="alert alert-success">
+                                                <p>
+                                                </p>
+                                            </div>
+
+                                            <div class="col-md-8 col-md-push-2 mt-10">
+                                                <div class="cursor-pointer pick-file" data-toggle="modal"
+                                                     data-target="#modal_full"
+                                                     style="border: dashed 2px #ddd; padding: 0 10px 10px 0; box-sizing: border-box;">
+                                                    <input class="image-file" type="hidden"
+                                                           name="s-image" value="">
+                                                    <div class="media stack-media-on-mobile">
+                                                        <div class="media-left">
+                                                            <div class="thumb">
+                                                                <a class="display-block"
+                                                                   style="-webkit-box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.4);-moz-box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.4);box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.4);">
+                                                                    <img
+                                                                            src=""
+                                                                            class="img-rounded" alt=""
+                                                                            style="width: 100px; height: 100px; object-fit: contain;"
+                                                                            data-base-url="">
+                                                                </a>
+                                                            </div>
+                                                        </div>
+
+                                                        <div class="media-body">
+                                                            <h6 class="media-heading">
+                                                                <a class="text-grey-300">
+                                                                    <span class="text-danger">*</span>
+                                                                    انتخاب تصویر:
+                                                                </a>
+                                                                <a class="io-image-name display-block">
+                                                                </a>
+                                                            </h6>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-12"></div>
+                                            <div class="form-group col-md-4 mt-10">
+                                                <label>
+                                                    <span class="text-danger">*</span>
+                                                    متن:
+                                                </label>
+                                                <input name="s-text"
+                                                       type="text"
+                                                       class="form-control"
+                                                       value="">
+                                            </div>
+                                            <div class="form-group col-md-3 mt-10">
+                                                <label>
+                                                    نام لینک:
+                                                </label>
+                                                <input name="s-btn-text"
+                                                       type="text"
+                                                       class="form-control"
+                                                       placeholder="مثال: درباره ما"
+                                                       value="">
+                                            </div>
+                                            <div class="form-group col-md-5 mt-10">
+                                                <label>
+                                                    آدرس لینک:
+                                                </label>
+                                                <input name="s-btn-url"
+                                                       type="url"
+                                                       class="form-control"
+                                                       placeholder="مثال: www.spsroham.ir/contactUs"
+                                                       value="">
+                                            </div>
+                                            <div class="text-right col-md-12">
+                                                <button type="submit" class="btn btn-primary">
+                                                    افزودن
+                                                    <i class="icon-arrow-left12 position-right"></i>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                                <!-- Standard width modal -->
+                                <div id="modal_full" class="modal fade">
+                                    <div class="modal-dialog modal-full">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <button type="button" class="close" data-dismiss="modal">&times;
+                                                </button>
+                                                <h5 class="modal-title">
+                                                    انتخاب فایل
+                                                </h5>
+                                            </div>
+
+                                            <div id="files-body" class="modal-body">
+                                                <?php include VIEW_PATH . 'templates/efm-view.php'; ?>
+                                            </div>
+
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-default" data-dismiss="modal">
+                                                    <i class="icon-cancel-circle2 position-left" aria-hidden="true"></i>
+                                                    لغو
+                                                </button>
+                                                <button id="file-ok" type="button" class="btn btn-primary"
+                                                        data-dismiss="modal">
+                                                    <i class="icon-checkmark-circle position-left"
+                                                       aria-hidden="true"></i>
+                                                    انتخاب
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- /standard width modal -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /form centered -->
+                <!-- Footer -->
+                <?php $this->view("templates/be/copyright", $data); ?>
+                <!-- /footer -->
+            </div>
+            <!-- /content area -->
+        </div>
+        <!-- /main content -->
+    </div>
+    <!-- /page content -->
+</div>
+<!-- /page container -->
